bad4fc24fc59ba1d1775e7e9e859be3042dbeab6
elseff <danilapetrov522@gmail.com>
2023-10-09 22:27:18 +0400

    Revert "fix filling db"
    This reverts commit 864c005b

--- src/main/resources/db/migration/V10_0__add_admin_role.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO role_entity(name)
-VALUES ('ROLE_ADMIN');
-
-INSERT INTO user_entity(id, username, first_name, last_name, password)
-VALUES (0, 'admin', 'admin', 'admin', 'admin');
-
-INSERT INTO user_entity_role_entity
-VALUES (0, 4);
