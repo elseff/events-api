693d59a785c274c39078d84edc5b9e363896eac1
elseff <danilapetrov522@gmail.com>
2023-11-12 19:36:19 +0400

    fix migrations

--- src/main/resources/db/migration/V11_0__change_user_sequece.sql
+++ src/main/resources/db/migration/V11_0__change_user_sequece.sql
@@ -1,2 +1,2 @@
-select setval('user_entity_id_seq', 4, true)
-select setval('hibernate_sequence', 4, true)
+select setval('user_entity_id_seq', 4, true);
+select setval('hibernate_sequence', 4, true);
