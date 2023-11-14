2573aa6605c3174208a94aed799762efdeed36de
Данила <danilapetrov522@gmail.com>
2023-11-14 16:05:32 +0400

    error handling

---
+++ src/main/java/ru/danila/eventsapi/exception/handling/dto/Violation.java
@@ -0,0 +1,19 @@
+package ru.danila.eventsapi.exception.handling.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Violation {
+
+    String fieldName;
+
+    String message;
+}
